f6154b0852528678d0c25eb0c2fb1704e4eb8b58
suelenquadros <suelen.quadros17@gmail.com>
2019-04-17 22:20:09 -0300

    Aula03

--- 02/Ex01-news/JS/Scrip.js
+++ 02/Ex01-news/JS/Scrip.js
@@ -6,8 +6,31 @@ var botao = document.querySelector('.button');
 botao.addEventListener('click', function(evento){
     evento.preventDefault();
     var email = document.querySelector('#newsInputEmail')
-    email.value
-    mensagem.textContent = 'O email '+ email.value +' foi cadastrado com sucesso!'
-    
-})
 
+
+if (email.value == "" || email.value.lenght == 0){
+
+mensagem.textContent = 'Digite um email válido'
+return
+
+
+} else if (email.value.indexOf ('@')== -1) {
+    mensagem.textContent = 'O email informado é inválido'
+return
+
+}
+else {
+    mensagem.textContent = 'O email' + email.value +' foi cadastrado com sucesso!'
+    console.log (email.value.indexOf ('@'))}
+}
+
+)
+
+
+
+// if (true && true)
+//  {console.log ('verdade verdadeira')}
+
+
+//  if (true || true)
+//  {console.log ('verdade verdadeira')}
